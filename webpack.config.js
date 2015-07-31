4c3fa1f099464541f9ca912c1a357715553deaab
young-utf <young.utf@gmail.com>
2015-08-01 05:05:46 +0900

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+/**
+ * Created by youngmoon on 8/1/15.
+ */
+
+module.exports = {
+    entry: "./app/App.js",
+    output: {
+        filename: 'public/bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel'
+            }
+        ]
+    }
+}
